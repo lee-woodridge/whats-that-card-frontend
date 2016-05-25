8ff9ab7092b8ba85a424570dfee6c5b55ab83bf1
Lee Woodridge <le3wood@gmail.com>
2016-05-24 22:06:51 -0700

    Fixed number-to-array spec file.

--- src/app/number-to-array.pipe.spec.ts
+++ src/app/number-to-array.pipe.spec.ts
@@ -10,7 +10,7 @@ import { NumberToArrayPipe } from './number-to-array.pipe';
 describe('NumberToArrayPipe Pipe', () => {
   beforeEachProviders(() => [NumberToArrayPipe]);
 
-  it('should transform the input', inject([NumberToArrayPipe], (pipe: NumberToArray) => {
-      expect(pipe.transform(true)).toBe(null);
+  it('should transform the input', inject([NumberToArrayPipe], (pipe: NumberToArrayPipe) => {
+      expect(pipe.transform(1)).toBe([1]);
   }));
 });
