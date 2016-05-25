e63c93d6b9ae1448210bc042c3644c97d0925c28
Lee Woodridge <le3wood@gmail.com>
2016-05-24 22:05:53 -0700

    Fixed number-to-array spec file.

--- src/app/number-to-array.pipe.spec.ts
+++ src/app/number-to-array.pipe.spec.ts
@@ -5,12 +5,12 @@ import {
   inject,
   beforeEachProviders
 } from '@angular/core/testing';
-import { NumberToArray } from './number-to-array.pipe';
+import { NumberToArrayPipe } from './number-to-array.pipe';
 
-describe('NumberToArray Pipe', () => {
-  beforeEachProviders(() => [NumberToArray]);
+describe('NumberToArrayPipe Pipe', () => {
+  beforeEachProviders(() => [NumberToArrayPipe]);
 
-  it('should transform the input', inject([NumberToArray], (pipe: NumberToArray) => {
+  it('should transform the input', inject([NumberToArrayPipe], (pipe: NumberToArray) => {
       expect(pipe.transform(true)).toBe(null);
   }));
 });
